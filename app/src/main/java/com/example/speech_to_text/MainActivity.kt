e06b50a91029b823639ff99e82a913cc156dc974
Aizen741 <rahulsivadas2009@gmail.com>
2020-01-13 00:25:22 +0530

    MainActivity

--- app/src/main/java/com/example/speech_to_text/MainActivity.kt
+++ app/src/main/java/com/example/speech_to_text/MainActivity.kt
@@ -1,12 +1,77 @@
 package com.example.speech_to_text
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    private val ASK_FOR_SPEECH = 100
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+     // When Button is clicked it will show Speed conversion to text
+
+        search_voice_btn.setOnClickListener{
+            speak()
+        }
+
+
     }
+    private fun speak(){
+        // I am adding an Intent here for the conversion to start
+
+        val speechIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,Locale.getDefault())
+        speechIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Hello there! Say Something..")
+
+        try{
+            // If Error do not Happen it will show speech to text
+
+            startActivityForResult(speechIntent,ASK_FOR_SPEECH)
+
+
+
+        }catch (e:Exception){
+
+            // If an Error Happens
+
+            Toast.makeText(this,e.message, Toast.LENGTH_SHORT).show()
+
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            ASK_FOR_SPEECH -> {
+                if (resultCode == Activity.RESULT_OK && null != data){
+                    val result = data.getStringArrayExtra(RecognizerIntent.EXTRA_RESULTS)
+
+
+                    textView.text = result[0]
+                }
+            }
+    }
+    }
+
+
+
 }
