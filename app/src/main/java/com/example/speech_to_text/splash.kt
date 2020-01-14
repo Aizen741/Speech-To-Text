2f2491014462cad5e0b6544b1f8bfe36ea0746fd
Aizen741 <rahulsivadas2009@gmail.com>
2020-01-14 21:43:16 +0530

    Basic Changes3

--- app/src/main/java/com/example/speech_to_text/splash.kt
+++ app/src/main/java/com/example/speech_to_text/splash.kt
@@ -1,5 +1,6 @@
 package com.example.speech_to_text
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,19 @@ class splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val background = object : Thread(){
+            override fun run(){
+                try{
+                    Thread.sleep(3000)
+
+                    val intent = Intent(baseContext,MainActivity::class.java)
+                    startActivity(intent)
+                }catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
     }
 }
